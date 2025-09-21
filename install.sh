9b2d5faf33bd58b7ec9fbfe04f9d7efc7c2cce08
Adrian Hossner <adhossner@gmail.com>
2025-09-21 22:53:27 +0200

    automatically make symlinks for config files

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create config directory if it doesn't exist
+mkdir -p "$HOME/.config"
+
+# Specify dotfiles directory
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# nvim
+ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+
+# tmux
+mkdir -p "$HOME/.config/tmux"
+ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.config/tmux/.tmux.conf"
+
+# zsh
+ln -sf "$DOTFILES_DIR/zsh/.zshrc" "$HOME/.zshrc"
+
+# git
+mkdir -p "$HOME/.config/git"
+ln -sf "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
+
+echo "Dotfiles installed!"
